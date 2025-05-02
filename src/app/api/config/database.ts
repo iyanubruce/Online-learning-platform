f7b72542440202d316febcaa4d0595c6e18eefaf
iyanubruce <iyanuoluwa938@gmail.com>
2025-05-02 16:11:00 +0100

    changed conncet to db function

--- src/app/api/config/database.ts
+++ src/app/api/config/database.ts
@@ -10,13 +10,18 @@ const mongoConfig = {
     process.env.MONGODB_URI ||
     `mongodb+srv://${database.username}:${database.password}@cluster0.udxp9.mongodb.net/OnlineLearningPlatform?retryWrites=true&w=majority&appName=Cluster0`,
 };
+
 const connectDB = async () => {
+  if (mongoose.connection.readyState) {
+    logger.info("Using existing database connection");
+    return;
+  }
   try {
     await mongoose.connect(mongoConfig.uri);
     logger.info("Connected to MongoDB");
   } catch (error) {
     logger.error("Error connecting to MongoDB:", error);
-    process.exit(1);
+    throw error;
   }
 };
 
